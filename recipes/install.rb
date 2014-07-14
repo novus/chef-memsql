b6ef50183b3d30c580408163401b3ee196d698fe
Steve Koppelman <skoppelman@novus.com>
2014-07-14 19:12:33 -0400

    GRR.

--- recipes/install.rb
+++ recipes/install.rb
@@ -48,7 +48,7 @@ remote_file "#{Chef::Config[:file_cache_path]}/memsql-#{node[:memsql][:version]}
   action :create_if_missing
 end
 
-dpkg_package "memsql-#{node[:memsql][:version]}" do
+dpkg_package "memsql" do
   source  "#{Chef::Config[:file_cache_path]}/memsql-#{node[:memsql][:version]}"
   action :install
 end
