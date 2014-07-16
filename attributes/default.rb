2d179a80a86f6976b79edd3c358f08242404cf8a
Steve Koppelman <skoppelman@novus.com>
2014-07-16 15:26:50 -0400

    ITSUP-1514 First pass at Ops installation.

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,14 +5,25 @@ default[:memsql][:gid] = node.memsql.uid
 default[:memsql][:license] = nil
 default[:memsql][:version] = "2.6.x86_64.deb"
 default[:memsql][:redundancy_level] = 1
-default[:memsql][:url]  = "http://download.memsql.com"
+default[:memsql][:url] = "http://download.memsql.com"
+default[:memsql][:users] = [{:name => 'developer', :password => 'password'}]
+default[:memsql][:node_scope][:enabled] = true
+default[:memsql][:node_scope][:filter] = " AND chef_environment:#{node.chef_environment}"
+
+# Backups
+
 default[:memsql][:backups][:databases] = []
 default[:memsql][:backups][:nfs_host] = nil
 default[:memsql][:backups][:nfs_path] = nil
 default[:memsql][:backups][:remote_mount_path] = nil
-default[:memsql][:users] = [{:name => 'developer', :password => 'password'}]
-default[:memsql][:node_scope][:enabled] = true
-default[:memsql][:node_scope][:filter] = " AND chef_environment:#{node.chef_environment}"
+
+# memsql-ops
+
 default[:memsql][:ops][:enabled] = false
+default[:memsql][:ops][:url] = 'http://download.memsql.com/ops-2.10.0'
+default[:memsql][:ops][:package] = 'memsql-ops-2.10.0.x86_64.deb'
+default[:memsql][:ops][:http_host] = '0.0.0.0'
+default[:memsql][:ops][:http_port] = 9000
+
 default[:memsql][:collectd][:url] = "http://download.memsql.com/ops-latest"
 default[:memsql][:collectd][:package] = "collectd-5.4.0.x86_64.deb"
--- recipes/ops.rb
+++ recipes/ops.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: memsql
-# Recipe:: collectd
+# Recipe:: ops
 #
 # Copyright 2014, Chris Molle
 #
@@ -17,22 +17,39 @@
 # limitations under the License.
 #        node.run_list.roles.include?('base')
 
-package 'libtool'
+ops_pkg = "#{Chef::Config[:file_cache_path]}/#{node[:memsql][:ops][:package]}"
 
-remote_file "#{Chef::Config[:file_cache_path]}/#{node[:memsql][:collectd][:package]}" do
-  source "#{node[:memsql][:url]}/#{node[:memsql][:collectd][:package]}"
+remote_file ops_pkg do
+  source "#{node[:memsql][:ops][:url]}/#{node[:memsql][:ops][:package]}"
   action :create_if_missing
 end
 
-dpkg_package node[:memsql][:collectd][:package] do
-  source  "#{Chef::Config[:file_cache_path]}/#{node[:memsql][:collectd][:package]}"
+dpkg_package 'ops' do
+  source ops_pkg
   action :install
 end
 
-template "/etc/collectd.conf" do
-  source "collectd.conf.erb"
+#find the master aggregator
+#TODO refactor
+filtered = node.memsql.node_scope.enabled ? node.memsql.node_scope.filter : ""
+master_aggregator = search(:node, "role:memsql_master_aggregator #{filtered}").first
+
+# What does this Ops instance monitor?
+# 1. If node[:memsql][:ops][:monitored_instance] contains a node, that's what it will be monitoring
+# 2. No? Look for a master aggregator in the same chef_environment, and monitor that, or
+# 3. Otherwise, assume this is a standalone and we want to monitor it
+monitored_instance = node.run_list.roles.include?("memsql_standalone") ? node || master_aggregator
+
+template "/var/lib/memsql-ops/memsql_ops.cnf" do
+  source "memsql_ops.cnf.erb"
   mode 0640
   variables({
-                :master_aggregator_ip => node.run_list.roles.include?("memsql_child_aggregator") ? master_aggregator["ipaddress"] : nil
+                :monitored_instance => monitored_instance
   })
+end
+
+#start ops
+service "memsql-ops" do
+  supports :status => true, :restart => true, :reload => true, :start => true, :stop => true
+  action [ :enable, :start ]
 end
